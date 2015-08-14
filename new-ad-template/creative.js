c6dbe7369346afc99dff478866f442c75aa9aa64
aaron stein <aaron@truex.com>
2015-08-13 19:06:25 -0700

    add starter template

---
+++ new-ad-template/creative.js
@@ -0,0 +1,24 @@
+(function() {
+
+	var creative = {
+		_init: function() {
+			TXM.dispatcher.addEventListenerOnce('ENGAGEMENT_STARTED', creative._onStart);
+			TXM.dispatcher.addEventListenerOnce('ENGAGEMENT_ENDED', creative._onEnd);
+
+			// preload assets
+
+			// when preload complete, signal ready to start
+			TXM.dispatcher.dispatchEvent('INTERACTIVE_ASSET_READY');
+		},
+
+		_onStart: function() {
+			// start ad
+		},
+
+		_onEnd: function() {
+			// end ad
+		}
+	};
+
+	creative._init();
+}());
